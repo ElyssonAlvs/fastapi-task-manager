f8230338d50b5634744c6de7644a9baa0f2e2ed1
ElyssonAlvs <elysson250102@gmail.com>
2026-03-24 00:05:34 -0300

    feat: implement database setup and integrate with FastAPI

--- app/main.py
+++ app/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+from app.database import Base, engine
+from app.routes import task as task_routes
 
 app = FastAPI(title="Task Manager API")
 
-@app.get("/")
+Base.metadata.create_all(bind=engine)
+
+@app.get("")
 def read_root():
     return {"message": "Task Manager API is running"}
+
+
+app.include_router(task_routes.router)
